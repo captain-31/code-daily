68b4e1cf951875b0861cf6798fcf412937dc3f4b
Hrishikesh <hp02.220@gmail.com>
2022-10-18 11:21:38 +0530

    Right angled type patterns solved

---
+++ patterns/pattern3.c
@@ -0,0 +1,25 @@
+/*
+Pattern 3: 
+
+54321
+54321
+54321
+54321
+54321
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1; i<=5; i++) {
+        for(j=5;j>=1;j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
